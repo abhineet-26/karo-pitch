5b377947a11db118281f25a54e8efde2f95fc8ea
Abhineet <194663976+abhineet-26@users.noreply.github.com>
2026-04-21 11:23:56 +0530

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: 'karo-pitch',
+    base: '/karo-pitch/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
